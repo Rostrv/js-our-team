bc5cb23df60e2d3a901e3dfa67f5c768e0bcf3d1
Rostrv <trovatorosario91@gmail.com>
2022-04-07 17:48:16 +0200

    add bonus

--- main.js
+++ main.js
@@ -32,11 +32,20 @@ const team = [{
 
 console.log(team)
 
+
+const members_list = document.getElementById("members")
+
 for (let i = 0; i < team.length; i++) {
-    const member = team[i]
+    let member = team[i]
+    divElement = document.createElement('div')
+    members_list.append(divElement)
+    divElement.innerHTML = `<div class="card mb-5"><img class="card-img-top mb-3" src="./img/${member.image}"></img>
+    <h4 class="text-center">${member.name}</h4>
+    <p class="text-center">${member.role}</p></div>`
+
     console.log(member.name)
     console.log(member.role)
-    console.log(member.image)
+    console.log(member['image'])
     console.log('--------')
 
 }
